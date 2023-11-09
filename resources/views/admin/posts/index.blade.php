6eb031929cc641dad2792c296cbed36f2805f39c
marcocentonze <marco.centonze27@gmail.com>
2023-11-09 15:26:31 +0100

    pagination ticket

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -39,70 +39,26 @@
                                 <td>{{ $project['title'] }}</td>
                                 <td>
                                     @if (str_contains($project['cover_image'], 'http'))
-                                        <img style="width:50px" class="img-fluid rounded-circle" src="{{ $comic['cover_image'] }}"
-                                            alt="{{ $project['title'] }}">
+                                        <img style="width:50px" class="img-fluid rounded-circle"
+                                            src="{{ $comic['cover_image'] }}" alt="{{ $project['title'] }}">
                                     @else
                                         <img style="width:50px" class="img-fluid rounded-circle"
-                                            src="{{ asset('storage/' . $project->cover_image) }}" alt="{{ $project['title'] }}">
+                                            src="{{ asset('storage/' . $project->cover_image) }}"
+                                            alt="{{ $project['title'] }}">
                                     @endif
                                 </td>
                                 <td>{{ $project['description'] }}</td>
-                                <td>
-                                    {{-- <a href="{{ route('show', $rpoject->id) }}"
-                                        class="btn btn-sm btn-secondary">More</a>
-                                    <a href="{{ route('edit', $project->id) }}" class="btn btn-sm btn-info">Edit</a> --}}
-
-
-                                    {{-- modal for delete button
-                                    <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                        data-bs-target="#modalId-{{ $comic->id }}">
-                                        Delete
-                                    </button> --}}
-
-                                    {{-- <!-- Modal Body -->
-                                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                                    <div class="modal fade" id="modalId-{{ $comic->id }}" tabindex="-1"
-                                        data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
-                                        aria-labelledby="modalTitle-{{ $comic->id }}" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
-                                            role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="modalTitle-{{ $comic->id }}">Modal id:
-                                                        {{ $comic->id }}</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                        aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    Attention! This is a destructive operation that cannot be undone.
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary"
-                                                        data-bs-dismiss="modal">Close</button>
-
-                                                    <!-- Delete form -->
-                                                    <form action="{{ route('comics.destroy', $comic->id) }}"
-                                                        method="POST">
-
-                                                        @csrf
-
-                                                        @method('DELETE')
-
-                                                        <button type="submit" class="btn btn-danger">Confirm</button>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div> --}}
-
-
-                                </td>
                             </tr>
                         @endforeach
+                        <!-- Pagination Links -->
+                        {{ $projects->links('pagination::bootstrap-5') }}
                     </tbody>
                 </table>
             </div>
+
+
         @endif
+
     </div>
 
 @endsection
