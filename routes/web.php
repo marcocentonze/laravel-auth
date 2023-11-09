4b25e4c324533190a3672d889c2e3fe166f015ce
marcocentonze <marco.centonze27@gmail.com>
2023-11-09 12:25:11 +0100

    add guest folder in view

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guests.welcome');
 });
 
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
